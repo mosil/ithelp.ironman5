3e2aa92413829db3f6b472784d0dc5161de4b644
Mosil <mosil.biz@gmail.com>
2012-10-21 00:23:17 +0800

    Day26_ContentProviders

---
+++ Day26_ContentProviders/src/demo/mosil/biz/in/ithelp/listview/Number.java
@@ -0,0 +1,8 @@
+package demo.mosil.biz.in.ithelp.listview;
+
+public class Number {
+	public static final int Chocobo = 0;
+	public static final int MOOGLE = 1;
+	public static final int MOOGLE2 = 2;
+	public static final int MOOGLE3 = 3;
+}
